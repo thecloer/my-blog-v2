b29cfbbd9e1d92210b378db0af717edbbebe7cf2
cloer <chltjrrb97@gmail.com>
2022-10-19 07:56:44 -0400

    UPDATE: tailwind.config + fontFamily, colors-primary

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          dark: colors.indigo,
+          light: colors.orange,
+        },
+      },
+    },
   },
   plugins: [],
 };
