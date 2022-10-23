7e24c6ac08ac6a410f8b39877f9aa77e0d82d1a2
cloer <chltjrrb97@gmail.com>
2022-10-23 18:11:57 -0400

    UPDATE: SocialIcon: primary color, add onClick

--- src/components/SocialIcon.tsx
+++ src/components/SocialIcon.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react';
+import type { FC, MouseEventHandler } from 'react';
 import { GithubIcon, InstagramIcon, LinkedinIcon, MailIcon, TwitterIcon } from '@/lib/svgs';
 
 const ICONS = {
@@ -13,19 +13,21 @@ type Props = {
   kind: keyof typeof ICONS;
   href: string;
   size?: number;
+  onClick?: MouseEventHandler<HTMLAnchorElement>;
 };
 
-const SocialIcon: FC<Props> = ({ kind, href, size = 6 }) => {
+const SocialIcon: FC<Props> = ({ kind, href, size = 6, onClick }) => {
   const SocialSvg = ICONS[kind];
 
   const specificClassName = kind === 'mail' ? '[&>*]:scale-y-110 [&>*]:scale-x-110' : '';
 
   return (
     <a
-      className='text-sm text-slate-600 transition-colors hover:text-primary-light-600 dark:text-slate-200 dark:hover:text-primary-dark-400'
+      className='text-sm text-slate-600 transition-colors hover:text-primary-100 dark:text-slate-200 dark:hover:text-primary-200'
       target='_blank'
       rel='noopener noreferrer'
       href={href}
+      onClick={onClick}
     >
       <span className='sr-only'>{kind}</span>
       <SocialSvg width={size * 4} height={size * 4} className={`fill-current ${specificClassName}`} />
