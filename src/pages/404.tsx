61c2e326e7cb9ae20c215dfa51c91e39189eecec
cloer <chltjrrb97@gmail.com>
2022-11-12 13:56:02 -0500

    ADD: 404 page simple

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const NotFoundPage: NextPage = () => {
+  return (
+    <div className='h-full'>
+      <h1>Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
