b709f56342bcd2f1d9fda61cb0d4e3f29e64df61
cloer <chltjrrb97@gmail.com>
2022-11-14 15:42:10 -0500

    ADD: svgs

--- src/lib/svgs/index.tsx
+++ src/lib/svgs/index.tsx
@@ -1,11 +1,14 @@
 // Icons taken from: https://simpleicons.org/
-
 import MailIcon from './mail.svg';
 import GithubIcon from './github.svg';
 import LinkedinIcon from './linkedin.svg';
 import TwitterIcon from './twitter.svg';
 import InstagramIcon from './instagram.svg';
+
 import HamburgerIcon from './hamburger-menu.svg';
 import XIcon from './x.svg';
+import Magnifier from './magnifier.svg';
+import PreviousArrow from './previousArrow.svg';
+import NextArrow from './nextArrow.svg';
 
-export { MailIcon, GithubIcon, LinkedinIcon, TwitterIcon, InstagramIcon, HamburgerIcon, XIcon };
+export { MailIcon, GithubIcon, LinkedinIcon, TwitterIcon, InstagramIcon, HamburgerIcon, XIcon, Magnifier, PreviousArrow, NextArrow };
