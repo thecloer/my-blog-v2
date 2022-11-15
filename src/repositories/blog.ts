c8045185ff9d5352457322ce5785859e5bdeb63d
cloer <chltjrrb97@gmail.com>
2022-11-14 21:17:57 -0500

    ADD: search (api, search bar, display search result)

--- src/repositories/blog.ts
+++ src/repositories/blog.ts
@@ -1,24 +1,35 @@
 import type { BlogFrontMatterWithSlug, SortFunc } from '@/types/types';
 import path from 'path';
 import dataPath from '@/config/dataPath';
-import { sortByFrontMatterDateDESC } from '@/lib/utils/sorter';
 import { getAllFilePathsRecursively, getBlogFrontMatterFromPath } from '@/lib/files';
+import { sortByFrontMatterDateDESC } from '@/lib/utils/sorter';
+import { blogSearchFilter } from '@/lib/utils/helper';
 
 export class Blog {
   private static _instance: Blog;
-  private readonly prefixPath: string;
-  private readonly filePaths: string[];
-  private readonly frontMatters: BlogFrontMatterWithSlug[];
+  private readonly _prefixPath: string;
+  private readonly _filePaths: string[];
+  private readonly _frontMatters: BlogFrontMatterWithSlug[];
+
   private constructor() {
-    this.prefixPath = dataPath.blog;
-    this.filePaths = getAllFilePathsRecursively(this.prefixPath).filter((filePath) => path.extname(filePath) === '.md' || path.extname(filePath) === '.mdx');
-    this.frontMatters = this.filePaths.map(getBlogFrontMatterFromPath).sort(sortByFrontMatterDateDESC);
+    this._prefixPath = dataPath.blog;
+    this._filePaths = getAllFilePathsRecursively(this._prefixPath).filter((filePath) => path.extname(filePath) === '.md' || path.extname(filePath) === '.mdx');
+    this._frontMatters = this._filePaths.map(getBlogFrontMatterFromPath).sort(sortByFrontMatterDateDESC);
   }
   static get instance() {
     return this._instance ?? (this._instance = new this());
   }
 
   getAllFrontMatters(sortFunc?: SortFunc<BlogFrontMatterWithSlug>) {
-    return sortFunc === undefined ? this.frontMatters : this.frontMatters.sort(sortFunc);
+    return sortFunc === undefined ? this._frontMatters : this._frontMatters.sort(sortFunc);
+  }
+
+  search(term: string): BlogFrontMatterWithSlug[];
+  search(terms: string[]): BlogFrontMatterWithSlug[];
+  search(term: string | string[]): BlogFrontMatterWithSlug[];
+  search(searchString: string | string[]) {
+    return typeof searchString === 'string'
+      ? this._frontMatters.filter(blogSearchFilter(searchString.toLowerCase()))
+      : this._frontMatters.filter((frontMatter) => searchString.every((term) => blogSearchFilter(term.toLowerCase())(frontMatter)));
   }
 }
