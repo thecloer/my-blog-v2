51d2a89b50738eff60164122e6f2e9a459fc4fb0
cloer <chltjrrb97@gmail.com>
2022-11-15 08:31:43 -0500

    UPDATE: remove return in arrow function

--- src/lib/utils/sorter.ts
+++ src/lib/utils/sorter.ts
@@ -1,5 +1,3 @@
 import type { CommonFrontMatter, SortFunc } from '@/types/types';
 
-export const sortByFrontMatterDateDESC: SortFunc<CommonFrontMatter> = (a, b) => {
-  return new Date(b.date).getTime() - new Date(a.date).getTime();
-};
+export const sortByFrontMatterDateDESC: SortFunc<CommonFrontMatter> = (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime();
