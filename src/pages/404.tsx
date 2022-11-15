0129e37c52ff970e48d1f33a81af9ec5ed4b8959
cloer <chltjrrb97@gmail.com>
2022-11-15 08:30:59 -0500

    UPDATE: 404 wrap AppWidthContainer

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next';
+import AppWidthContainer from '@/containers/AppWidthContainer';
 
 const NotFoundPage: NextPage = () => {
   return (
-    <div className='h-full'>
-      <h1>Not Found</h1>
-    </div>
+    <AppWidthContainer>
+      <h1 className='text-2xl font-bold'>Not Found</h1>
+    </AppWidthContainer>
   );
 };
 
