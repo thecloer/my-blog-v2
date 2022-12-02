123b77c48b7c0bba2fd07588a6c67b346819fea9
cloer <chltjrrb97@gmail.com>
2022-12-02 17:11:22 -0500

    DELETE: BlogPostListLayout -> marge to [page_number] page

--- src/pages/blog/page/[page_number].tsx
+++ src/pages/blog/page/[page_number].tsx
@@ -1,11 +1,17 @@
 import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import type { ParsedUrlQuery } from 'querystring';
 import type { BlogFrontMatterWithSlug } from '@/types/data.type';
-import { POSTS_PER_PAGE } from '@/config/layoutConfig';
-import { Blog } from '@/repositories/blog';
-import BlogPostListLayout from '@/layouts/BlogPostListLayout';
-import { SearchProvider } from '@/contexts/searchContext';
+import { useEffect, useState } from 'react';
 import AppWidthContainer from '@/containers/AppWidthContainer';
+import BlogSidebar from '@/components/sidebar/BlogSidebar';
+import BlogInlineSidebar from '@/components/sidebar/BlogInlineSidebar';
+import PostList from '@/components/PostList';
+import Pagination from '@/components/pagination/Pagination';
+import { SearchProvider, useSearchContext } from '@/contexts/searchContext';
+import useDebounce from '@/hooks/useDebounce';
+import { POSTS_PER_PAGE } from '@/config/layoutConfig';
+import urlPath from '@/config/urlPath';
+import { Blog } from '@/repositories/blog';
 
 type Props = {
   allPostNumber: number;
@@ -19,10 +25,35 @@ interface Params extends ParsedUrlQuery {
 }
 
 const BlogPage: NextPage<Props> = ({ allPostNumber, initialDisplayPosts, currentPage, lastPage }) => {
+  const { searchString } = useSearchContext();
+  const [searchResult, setSearchResult] = useState<BlogFrontMatterWithSlug[]>([]);
+
+  const debouncedSearchTerm = useDebounce(searchString);
+  const isInitialPage = debouncedSearchTerm.length < 2;
+  const displayPosts = isInitialPage ? initialDisplayPosts : searchResult;
+
+  useEffect(() => {
+    if (isInitialPage) return;
+    const searchPosts = async () => {
+      const res = await fetch(urlPath.apiBlogSearch(encodeURIComponent(debouncedSearchTerm)));
+      const { result } = await res.json();
+      setSearchResult(result);
+    };
+    searchPosts();
+  }, [isInitialPage, debouncedSearchTerm]);
+
   return (
     <AppWidthContainer>
       <SearchProvider>
-        <BlogPostListLayout allPostNumber={allPostNumber} initialDisplayPosts={initialDisplayPosts} currentPage={currentPage} lastPage={lastPage} />
+        <div className='flex'>
+          <BlogSidebar />
+          <main className='mt-6 mb-8 grow'>
+            <h1 className='mb-10 text-5xl font-extrabold'>{isInitialPage ? `All Posts: ${allPostNumber}` : `Results: ${searchResult.length}`}</h1>
+            <BlogInlineSidebar />
+            <PostList posts={displayPosts} />
+            {isInitialPage ? <Pagination currentPage={currentPage} lastPage={lastPage} /> : null}
+          </main>
+        </div>
       </SearchProvider>
     </AppWidthContainer>
   );
