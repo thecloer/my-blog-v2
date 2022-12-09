353cc86f7cb27f486d2ef1284e93d155b3529935
cloer <chltjrrb97@gmail.com>
2022-12-09 16:10:30 -0500

    UPDATE: encode & decode uri + slug

--- src/config/urlPath.ts
+++ src/config/urlPath.ts
@@ -1,9 +1,11 @@
+import { encodeURISlug } from '@/lib/utils/formatter';
+
 const urlPath = {
   apiBlogSearch: (query: string) => `/api/blog/search?q=${encodeURIComponent(query)}`,
-  blogPage: (page_number: number) => `/blog/page/${page_number}`,
+  blogPage: (pageNumber: number) => `/blog/page/${pageNumber}`,
   blogPost: (slug: string) => `/blog/post/${slug}`,
-  blogTag: (tag: string) => `/blog/tag/${tag}`,
-  blogSeries: (series: string) => `/blog/series/${series}`,
+  blogTag: (tag: string) => `/blog/tag/${encodeURISlug(tag)}`,
+  blogSeries: (series: string) => `/blog/series/${encodeURISlug(series)}`,
 } as const;
 
 export default urlPath;
