3125b7de15bf6ec6f5566504c7f44f2d7ef9c04f
cloer <chltjrrb97@gmail.com>
2022-12-11 12:01:27 -0500

    UPDATE: tagButton color

--- src/components/TagButton.tsx
+++ src/components/TagButton.tsx
@@ -11,7 +11,7 @@ const TagButton: FC<Props> = ({ name, count }) => {
   const text = count === undefined ? name : `${name} (${count})`;
   return (
     <Link href={urlPath.blogTag(name)}>
-      <a className='rounded bg-primary-400 px-1 py-[1px] text-sm hover:bg-primary-300 dark:text-bgDark-800 hover:dark:bg-primary-500'>
+      <a className='rounded bg-primary-400 px-1 py-[1px] text-sm hover:bg-primary-300 dark:bg-primary-500 dark:text-bgDark-800 hover:dark:bg-primary-400'>
         {name}
         {count !== undefined ? <span className='ml-1 text-xs'>{count}</span> : null}
       </a>
