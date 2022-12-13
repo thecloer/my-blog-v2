2f0237eaac6d32bd353dc177bbfe13500bf89e44
cloer <chltjrrb97@gmail.com>
2022-12-13 17:58:52 -0500

    FIX: api decode bug fix

--- src/pages/api/blog/search/tags.ts
+++ src/pages/api/blog/search/tags.ts
@@ -1,14 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Blog } from '@/repositories/blog';
+import { decodeURISlug } from '@/lib/utils/formatter';
 
 export default async function tagsHandler(req: NextApiRequest, res: NextApiResponse) {
   const searchQuery = req.query.tags;
   if (searchQuery === undefined) return res.status(400).json({ result: [] });
 
   const tags =
-    typeof searchQuery === 'string'
-      ? decodeURIComponent(searchQuery)
-      : searchQuery.map((query) => decodeURIComponent(query));
+    typeof searchQuery === 'string' ? decodeURISlug(searchQuery) : searchQuery.map((query) => decodeURISlug(query));
 
   const result = Blog.getFrontMattersByTag(tags);
 
