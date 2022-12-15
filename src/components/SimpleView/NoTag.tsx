1df190b82dad6f437408810ceaaf2d591bd9379b
cloer <chltjrrb97@gmail.com>
2022-12-15 15:44:59 -0500

    UPDATE: NoTag component

--- src/components/SimpleView/NoTag.tsx
+++ src/components/SimpleView/NoTag.tsx
@@ -1,8 +1,7 @@
 const NoTag = () => {
   return (
     <div className='flex h-full w-full flex-col items-center justify-center'>
-      <h1 className='text-4xl font-bold'>No Post</h1>
-      <p className='mt-4 text-xl'>Please select tags</p>
+      <p className='text-xl'>Please select tags</p>
     </div>
   );
 };
