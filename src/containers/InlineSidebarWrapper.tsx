77090b9fb867c91370bad6e54c24e5d1669fc912
cloer <chltjrrb97@gmail.com>
2022-12-16 00:10:51 -0500

    UPDATE: inlineSidebar bg color

--- src/containers/InlineSidebarWrapper.tsx
+++ src/containers/InlineSidebarWrapper.tsx
@@ -16,8 +16,8 @@ const InlineSidebarWrapper: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className='relative z-10 mb-8 block lg:hidden' ref={inlineSidebarRef}>
       <button
-        className={`flex h-10 w-full items-center gap-2 rounded-md bg-bgDark-200 px-3 text-lg font-medium ${
-          showChildren ? 'dark:bg-bgDark-700' : 'dark:bg-bgDark-800'
+        className={`flex h-10 w-full items-center gap-2 rounded-md px-3 text-lg font-medium ${
+          showChildren ? 'bg-bgDark-200 dark:bg-bgDark-700' : 'bg-bgDark-300 dark:bg-bgDark-800'
         }`}
         onClick={toggleShowChildren}
       >
@@ -27,7 +27,7 @@ const InlineSidebarWrapper: FC<PropsWithChildren> = ({ children }) => {
         Menu
       </button>
       {showChildren && (
-        <div className='absolute top-4 -z-10 w-full rounded-b-md bg-bgDark-100 px-5 pt-10 pb-5 dark:bg-bgDark-700'>
+        <div className='absolute top-4 -z-10 w-full rounded-b-md bg-bgDark-200 px-5 pt-10 pb-5 dark:bg-bgDark-700'>
           {children}
         </div>
       )}
