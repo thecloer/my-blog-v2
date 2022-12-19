157f4c55bb314f350a9a9e10b9e467273eb421b2
cloer <chltjrrb97@gmail.com>
2022-12-19 03:06:10 -0500

    UPDATE: header: make navButton inactive when it links to the current page

--- src/components/header/HeaderNav.tsx
+++ src/components/header/HeaderNav.tsx
@@ -1,16 +1,36 @@
+import { useCallback } from 'react';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import headerNavLinks from '@/config/headerNavLinks';
+import urlPath from '@/config/urlPath';
 
 const HeaderNav = () => {
+  const { asPath } = useRouter();
+  const isCurrentLink = useCallback(
+    (href: string) =>
+      href === urlPath.blog.index
+        ? asPath === urlPath.root ||
+          asPath === urlPath.blog.index ||
+          asPath.startsWith(urlPath.blog.page(0).slice(0, -1))
+        : asPath.startsWith(href),
+    [asPath]
+  );
+
   return (
     <nav className='hidden sm:flex sm:items-center'>
-      {headerNavLinks.map((link) => (
-        <Link key={link.title} href={link.href} passHref>
-          <a className='p-4 text-base font-semibold capitalize text-slate-600 transition-colors hover:text-primary-500 dark:text-slate-200 hover:dark:text-primary-400'>
-            {link.title}
+      {headerNavLinks.map(({ href, title }) =>
+        isCurrentLink(href) ? (
+          <a className='cursor-default p-4 text-base font-semibold capitalize text-primary-500 dark:text-primary-400'>
+            {title}
           </a>
-        </Link>
-      ))}
+        ) : (
+          <Link key={title} href={href}>
+            <a className='p-4 text-base font-semibold capitalize text-bgDark-600 transition-colors hover:text-primary-500 dark:text-bgDark-200 hover:dark:text-primary-400'>
+              {title}
+            </a>
+          </Link>
+        )
+      )}
     </nav>
   );
 };
