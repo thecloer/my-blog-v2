8dd14f0f9c8b371e1b37cf30224793b17bd2ddbb
cloer <chltjrrb97@gmail.com>
2023-01-07 15:11:46 -0500

    UPDATE: scrollbar-thin postcss

--- src/layouts/ContentWithSidebarLayout.tsx
+++ src/layouts/ContentWithSidebarLayout.tsx
@@ -7,7 +7,7 @@ type Props = {
 const ContentWithSidebarLayout: FC<PropsWithChildren<Props>> = ({ sidebar, children }) => {
   return (
     <div className='flex'>
-      <aside className='sticky top-[6.5rem] mr-4 hidden max-h-[calc(100vh-6.5rem)] w-[300px] shrink-0 flex-col overflow-y-scroll pr-6 lg:flex'>
+      <aside className='scrollbar-thin sticky top-[6.5rem] mr-4 hidden max-h-[calc(100vh-6.5rem)] w-[300px] shrink-0 flex-col overflow-y-scroll pr-6 lg:flex'>
         {sidebar}
       </aside>
       <section className='grow pb-8'>{children}</section>
