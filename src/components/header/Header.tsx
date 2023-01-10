b9d3b77bfdd40a86e43eebd4760fdebbfcec5a21
cloer <chltjrrb97@gmail.com>
2023-01-10 18:07:41 -0500

    FIX: git config core.ignorecase false

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import siteMetadata from '@/config/siteMetadata';
+import AppWidthContainer from '@/containers/AppWidthContainer';
+import HeaderNav from './HeaderNav';
+import ThemeSwitch from './ThemeSwitch';
+import MobileNav from './MobileNav';
+
+const Header = () => {
+  return (
+    <header className='sticky top-0 z-20 bg-white/80 py-6 backdrop-blur dark:bg-bgDark-900/80'>
+      <AppWidthContainer>
+        <div className='flex items-center justify-between'>
+          <Link href='/' aria-label={siteMetadata.title} passHref>
+            <a className='flex items-center gap-x-2'>
+              <Image
+                src='/images/logos/cloer-logo-indigo-512x512.png'
+                layout='fixed'
+                width='40'
+                height='40'
+                alt='blog logo'
+              />
+              <span className='hidden text-2xl font-semibold sm:block'>{siteMetadata.title}</span>
+            </a>
+          </Link>
+
+          <div className='flex items-center'>
+            <HeaderNav />
+            <ThemeSwitch />
+            <MobileNav />
+          </div>
+        </div>
+      </AppWidthContainer>
+    </header>
+  );
+};
+
+export default Header;
