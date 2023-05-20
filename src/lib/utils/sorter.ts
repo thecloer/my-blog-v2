78f9fede7c670affbb87b6440885a29f1e8f84f8
cloer <chltjrrb97@gmail.com>
2023-05-20 14:24:16 +0900

    FIX: hoistItem

--- src/lib/utils/sorter.ts
+++ src/lib/utils/sorter.ts
@@ -4,7 +4,5 @@ import type { SortFunc } from '@/types/utils.type';
 export const sortByFrontMatterDateDESC: SortFunc<CommonFrontMatter> = (a, b) =>
   new Date(b.date).getTime() - new Date(a.date).getTime();
 
-export const hoistItem = <T>(list: T[], items: T[]) => {
-  const newList = list.filter((i) => !items.includes(i));
-  return [...items, ...newList];
-};
+export const hoistItem = <T>(list: T[], items: T[]) =>
+  list.reduce((newList, item) => (items.includes(item) ? [...newList, item] : [item, ...newList]), [] as T[]);
