6804babf496c3f4fb8c9370f5c75c1ccc235afef
cloer <chltjrrb97@gmail.com>
2023-06-13 06:25:25 +0900

    UPDATE: sitemap config changefreq monthly

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: require('./package.json').homepage || 'https://www.thecloer.com',
+  changefreq: 'monthly',
   generateRobotsTxt: true,
 };
