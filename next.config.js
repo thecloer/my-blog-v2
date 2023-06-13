4dd124efeb38e677d11be692d0ccb4262c9710f2
cloer <chltjrrb97@gmail.com>
2023-06-13 10:34:55 +0900

    ADD: preact

--- next.config.js
+++ next.config.js
@@ -2,12 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config, { defaultLoaders }) {
+  webpack(config, { defaultLoaders, isServer, dev }) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: [defaultLoaders.babel, { loader: '@svgr/webpack' }],
     });
+
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
     return config;
   },
 };
